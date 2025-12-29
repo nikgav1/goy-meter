3d7a52bf1464ec38d7724dcd6fb7bdba11c7272c
nikgav1 <nikgav66@gmail.com>
2025-12-29 17:12:52 +0300

    Goy interface

--- goy.py
+++ goy.py
@@ -1,2 +1,13 @@
 # Create goy meter that will use photo how much red40 there is the food
-print("goy")
+from PIL import Image
+import tkinter as tk
+
+def calculate_amount_red40():
+    print("afdinef")
+
+def main():
+    r = tk.Tk()
+    r.title('Goy Meter')
+    r.mainloop()
+
+main()
